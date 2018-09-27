df8ed72b230e05a53d7d466d047e4230bd2c9945
kolomu <kevin-olomu@web.de>
2018-09-27 19:51:31 +0200

    removed unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
-import { Component, EventEmitter, ChangeDetectorRef, OnInit } from '@angular/core';
-import { distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
-import { DataService } from './data.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   cities = [
     {id: 1, name: 'Vilnius', avatar: '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x'},
     {id: 2, name: 'Kaunas', avatar: '//www.gravatar.com/avatar/ddac2aa63ce82315b513be9dc93336e5?d=retro&r=g&s=15'},
@@ -15,48 +13,6 @@ export class AppComponent implements OnInit {
     {id: 4, name: 'Siauliai', avatar: '//www.gravatar.com/avatar/b0d8c6e5ea589e6fc3d3e08afb1873bb?d=retro&r=g&s=30 2x'},
 ];
 
-cities2 = this.cities.slice();
-cities3 = this.cities.slice();
-cities4 = this.cities.slice();
-
 selectedCity = this.cities[0].name;
-selectedCity2 = this.cities2[1].name;
-selectedCity3 = this.cities3[2].name;
 
-people = [];
-selectedPeople = [];
-serverSideFilterItems = [];
-
-peopleTypeahead = new EventEmitter<string>();
-
-constructor(private dataService: DataService, private cd: ChangeDetectorRef) {}
-
-ngOnInit() {
-    this.dataService.getPeople().subscribe(items => {
-        this.people = items;
-    });
-    this.serverSideSearch();
-}
-
-selectAll() {
-    this.selectedPeople = this.people.map(x => x.name);
-}
-
-unselectAll() {
-    this.selectedPeople = [];
-}
-
-private serverSideSearch() {
-    this.peopleTypeahead.pipe(
-        distinctUntilChanged(),
-        debounceTime(300),
-        switchMap(term => this.dataService.getPeople(term))
-    ).subscribe(x => {
-        this.cd.markForCheck();
-        this.serverSideFilterItems = x;
-    }, (err) => {
-        console.log(err);
-        this.serverSideFilterItems = [];
-    });
-}
 }
